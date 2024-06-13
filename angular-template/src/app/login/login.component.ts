dcfb2d9001bf1e029e93db2d3cb872af128d2e48
Felix2680 <isaac7566@gmail.com>
2024-06-12 18:51:59 -0600

    mejoras en el diseño
    agregué mas diseño a la pantalla de login, cambiando algunas que ya tenia anteriormente

--- angular-template/src/app/login/login.component.ts
+++ angular-template/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from './longin.service';
 import { Router } from '@angular/router';
-
+import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,34 +9,49 @@ import { Router } from '@angular/router';
 })
 
 export class LoginComponent implements OnInit {
- username:string="";
- password:string="";
- constructor(/*private _loginService: LoginService, private router: Router*/){
-   
- }
- ngOnInit(){
-/*let token =  localStorage.getItem('token');
-  // localStorage.getItem("token")
-  if(token!=null){
-    this.router.navigate(['/products']);
+  username: string = "";
+  password: string = "";
+  usuario = {
+    email: '',
+    password: ''
+  };
+  passwordVisible = false;
+  showPasswordIcon = false;
+  
+  constructor(/*private _loginService: LoginService, private router: Router*/) {
+
   }
-  */
- }
- login(){
-  /*console.log(this.username);
-  console.log(this.password);
-  const data={
-    username: this.username,
-    password: this.password
+
+  enviarDatos() {
+    console.log(this.usuario)
   }
-  this._loginService.login(data).subscribe({next:(res)=>{
-    console.log(res)
-    localStorage.setItem("token",res.token) //valida el token
-    this.router.navigate(['/products']); //redirige a productos
-  },error:(err)=>{
-    console.log(err)
-    alert(err.error.message)
-  }});*/
- }
+
+  verContrasenia() {
+    this.passwordVisible = !this.passwordVisible;
+  }
+  ngOnInit() {
+    /*let token =  localStorage.getItem('token');
+      // localStorage.getItem("token")
+      if(token!=null){
+        this.router.navigate(['/products']);
+      }
+      */
+  }
+  login() {
+    /*console.log(this.username);
+    console.log(this.password);
+    const data={
+      username: this.username,
+      password: this.password
+    }
+    this._loginService.login(data).subscribe({next:(res)=>{
+      console.log(res)
+      localStorage.setItem("token",res.token) //valida el token
+      this.router.navigate(['/products']); //redirige a productos
+    },error:(err)=>{
+      console.log(err)
+      alert(err.error.message)
+    }});*/
+  }
 }
 
