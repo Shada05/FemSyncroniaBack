866a86b23c690c61576ced6db41895b962880e3b
Felix2680 <isaac7566@gmail.com>
2024-09-05 23:10:22 -0600

    Pantalla inicio
    migre la pantalla que se presenta al inicio

--- front-femSyncronia/src/app/app-routing.module.ts
+++ front-femSyncronia/src/app/app-routing.module.ts
@@ -8,8 +8,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
 ];
 
