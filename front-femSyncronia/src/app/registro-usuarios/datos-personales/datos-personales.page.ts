8528d0c113c8ac5cfc04e89e7ead8edbcdbd5600
Felix2680 <isaac7566@gmail.com>
2024-10-01 02:22:57 -0600

    estilo Pantalla datos personales
    agregué las dependencias necesarias para la foto de perfil, además de agregar todo el estilo de la pantalla

--- front-femSyncronia/src/app/registro-usuarios/datos-personales/datos-personales.page.ts
+++ front-femSyncronia/src/app/registro-usuarios/datos-personales/datos-personales.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-datos-personales',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatosPersonalesPage implements OnInit {
 
+  profileImage: string = '../../../assets/img/registro/defaul-perfil.svg';
+
+  // Variables para los selects
+  selectedDay: number = 0; // Día actual por defecto
+  selectedMonth: string = ''; // Inicializa la variable
+  selectedYear: number = 0; // Año actual por defecto
+  selectedLada: string = '';
+  days: number[] = Array.from({ length: 31 }, (v, k) => k + 1); // Días 1-31
+  months: string[] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+  years: number[] = Array.from({ length: 124 }, (v, k) => k + 1900); // Años 1900-2023
+
   constructor() { }
 
   ngOnInit() {
+    this.selectedLada = '+52';
   }
 
+  // Función para cambiar la foto de perfil
+  async changeProfilePicture() {
+    try {
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Photos // Abre la galería
+      });
+
+      // Verificamos si image.dataUrl tiene un valor
+      if (image && image.dataUrl) {
+        this.profileImage = image.dataUrl;
+      } else {
+        console.log('No se seleccionó ninguna imagen.');
+      }
+    } catch (error) {
+      console.log('Error al seleccionar la imagen: ', error);
+    }
+  }
 }
