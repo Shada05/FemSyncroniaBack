0627f9c5e45641ba28895e332b8b525834c0abee
Felix2680 <isaac7566@gmail.com>
2024-10-15 00:11:38 -0600

    primeras dos preguntas
    agregue las dos primeras preguntas con los downdrops listos

--- front-femSyncronia/src/app/formulario/cuentanos/cuentanos.page.ts
+++ front-femSyncronia/src/app/formulario/cuentanos/cuentanos.page.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cuentanos.page.scss'],
 })
 export class CuentanosPage implements OnInit {
+  anioActual: number = 0;
+  anios: number[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.anioActual = new Date().getFullYear();
+    this.generarAnios();
   }
 
+  generarAnios() {
+    // Rango de edad: 11 a 56 años
+    const edadMinima = 11;
+    const edadMaxima = 56;
+
+    for (let edad = edadMinima; edad <= edadMaxima; edad++) {
+      this.anios.push(this.anioActual - edad);
+    }
+  }
 }
