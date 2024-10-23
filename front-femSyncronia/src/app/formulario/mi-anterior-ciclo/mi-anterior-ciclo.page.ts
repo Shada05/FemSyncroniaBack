af8ff78d18acaa49042452200db9d2ef1049e83c
Felix2680 <isaac7566@gmail.com>
2024-10-22 21:46:39 -0600

    Pantalla mi primer ciclo
    agregue la 3 primeras preguntas

--- front-femSyncronia/src/app/formulario/mi-anterior-ciclo/mi-anterior-ciclo.page.ts
+++ front-femSyncronia/src/app/formulario/mi-anterior-ciclo/mi-anterior-ciclo.page.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mi-anterior-ciclo.page.scss'],
 })
 export class MiAnteriorCicloPage implements OnInit {
+  meses: string[] = ['Ene.', 'Feb.', 'Mar.', 'Abr.', 'May.', 'Jun.', 'Jul.', 'Ago.', 'Sep.', 'Oct.', 'Nov.', 'Dic.'];
+  dias: number[] = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  // Actualiza los días dependiendo del mes seleccionado
+  actualizarDias(event: any) {
+    const mesSeleccionado = event.detail.value;
+
+    if (mesSeleccionado === 'Febrero') {
+      this.dias = Array.from({ length: 28 }, (_, i) => i + 1); // 28 días para febrero
+    } else if (['Abril', 'Junio', 'Septiembre', 'Noviembre'].includes(mesSeleccionado)) {
+      this.dias = Array.from({ length: 30 }, (_, i) => i + 1); // 30 días para meses con 30 días
+    } else {
+      this.dias = Array.from({ length: 31 }, (_, i) => i + 1); // 31 días para los demás meses
+    }
+  }
 }
