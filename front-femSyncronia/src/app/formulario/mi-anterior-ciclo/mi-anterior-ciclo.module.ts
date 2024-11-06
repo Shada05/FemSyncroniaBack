8bded5ee1e7c6d6f8255cee50e928786ccd46928
Felix2680 <isaac7566@gmail.com>
2024-11-06 00:03:10 -0600

    validaciones
    Agregue las validaciones para los dos inputs

--- front-femSyncronia/src/app/formulario/mi-anterior-ciclo/mi-anterior-ciclo.module.ts
+++ front-femSyncronia/src/app/formulario/mi-anterior-ciclo/mi-anterior-ciclo.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 import { MiAnteriorCicloPageRoutingModule } from './mi-anterior-ciclo-routing.module';
@@ -13,7 +12,8 @@ import { MiAnteriorCicloPage } from './mi-anterior-ciclo.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    MiAnteriorCicloPageRoutingModule
+    MiAnteriorCicloPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [MiAnteriorCicloPage]
 })
