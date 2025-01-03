5981633905aba7c0aef3ffee333a938948f5f59f
Felix2680 <isaac7566@gmail.com>
2025-01-03 00:07:15 -0600

    Scroll invasivo
    Arreglé un bug que hacia que al hacer focus en un input, el código hacia un scroll hacia arriba

--- front-femSyncronia/src/app/app.module.ts
+++ front-femSyncronia/src/app/app.module.ts
@@ -10,8 +10,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot({
+    mode: 'ios',
+    scrollAssist: false,
+    scrollPadding: false
+  }), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
