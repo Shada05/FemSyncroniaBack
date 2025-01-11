7bfb94af387fdccb6ac74c36d74f4694c7fe2109
Felix2680 <isaac7566@gmail.com>
2025-01-11 00:12:24 -0600

    Barra lateral
    Agregué la barra lateral que sale al dar clic en la imagen de perfil, ya tiene los estilos y los iconos correspondientes. Falta la funcionalidad

--- front-femSyncronia/src/app/pantalla-principal/inicio/inicio.page.ts
+++ front-femSyncronia/src/app/pantalla-principal/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular'; // Importa el controlador del menú
 
 @Component({
   selector: 'app-inicio',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-  profileImage: string = '/assets/img/registro/defaul-perfil.svg';
-  constructor() { }
+  profileImage: string = '/assets/img/pantalla-principal/Foto-perfil.svg'; // Ruta de la imagen de perfil
 
-  ngOnInit() {
+  constructor(private menuCtrl: MenuController) { } // Inyecta el controlador del menú
+
+  ngOnInit() { }
+
+  /**
+   * Abre el menú lateral cuando se hace clic en la imagen de perfil
+   */
+  abrirMenuPerfil() {
+    this.menuCtrl.open('menu-perfil'); // Abre el menú con el ID 'menu-perfil'
   }
 
+  /**
+   * Cierra el menú lateral si está abierto
+   */
+  cerrarMenuPerfil() {
+    this.menuCtrl.close('menu-perfil'); // Cierra el menú con el ID 'menu-perfil'
+  }
+
+  /**
+   * Comprueba si el menú lateral está abierto
+   */
+  async isMenuOpen(): Promise<boolean> {
+    return this.menuCtrl.isOpen('menu-perfil'); // Retorna el estado del menú con el ID 'menu-perfil'
+  }
 }
