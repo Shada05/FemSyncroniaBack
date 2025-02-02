dd86e07171e6fd3a4686fedce71f27e2c9f36e19
Felix2680 <isaac7566@gmail.com>
2025-02-02 16:09:05 -0600

    path
    el path para buscar el apiService era incorrecto

--- front-femSyncronia/src/app/pantalla-principal/inicio/inicio.page.ts
+++ front-femSyncronia/src/app/pantalla-principal/inicio/inicio.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular'; // Importa el controlador del menú
-import { ApiService } from '../../api.service';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-inicio',
