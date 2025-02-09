f80e8a7134b8d104c66b7a8ed653d83a43f226a1
Felix2680 <isaac7566@gmail.com>
2025-02-08 19:10:39 -0600

    Sebreescritura
    Habia un conflicto junto con la funcion que busca por id

--- api-femSyncronia/routes/index.js
+++ api-femSyncronia/routes/index.js
@@ -60,7 +60,7 @@ router.post('/api/v1/users', usersController.store);
 // Ruta para listar todos los usuarios
 router.get('/api/v1/users', usersController.index);
 // Ruta para mostrar un solo usuario por ID
-router.get('/api/v1/users/:email', usersController.show_email);
+//router.get('/api/v1/users/:email', usersController.show_email);
 router.get('/api/v1/users/:id', usersController.show);
 // Ruta para actualizar un usuario
 router.put('/api/v1/users/:id', usersController.update);
