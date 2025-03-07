b3e85fa85fd169cad87a926f559b6d827a5a9a7f
Isaac Feliciano Hernández Rivera <Feliciano.Hernandez@alumno.udg.mx>
2025-03-06 23:29:23 -0600

    persiste el error en la api

--- api-femSyncronia/index.js
+++ api-femSyncronia/index.js
@@ -4,46 +4,36 @@ const logger = require("morgan");
 const http = require('http');
 const dotenv = require('dotenv');
 const cors = require('cors'); // Importa cors
-const multer = require('multer');
-const storage = multer.memoryStorage(); // Almacena en memoria
-const upload = multer({ storage: storage });
 
-
-/*app.post('/api/upload', (req, res) => {
-    console.log(req.body);
-    console.log(req.files);
-    res.send('Archivo recibido');
-});*/
 // Configurar dotenv
 dotenv.config();
 
 // Configurar la aplicación Express
 const app = express();
-let { router } = require('./routes');
 
 // Lista de orígenes permitidos
 const allowedOrigins = [
     'http://localhost:8100', // Desarrollo con Ionic serve
     'capacitor://localhost', // Origen en Android
     'http://localhost',      // Origen en iOS
+    'https://localhost',     // Origen en Android Studio
 ];
 
 // Configurar CORS
 app.use(cors({
     origin: function (origin, callback) {
-        // Permitir solicitudes sin origen (como aplicaciones móviles o Postman)
+        console.log('Origen de la solicitud:', origin); // Depuración
         if (!origin) return callback(null, true);
 
-        // Verificar si el origen está en la lista de permitidos
         if (allowedOrigins.includes(origin)) {
             return callback(null, true);
         } else {
             return callback(new Error('Origen no permitido por CORS'));
         }
     },
-    methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
-    allowedHeaders: 'Content-Type,Authorization', // Cabeceras permitidas
-    credentials: true, // Permitir credenciales (cookies, tokens, etc.)
+    methods: 'GET,POST,PUT,DELETE,OPTIONS', // Asegúrate de incluir OPTIONS
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
 }));
 
 // Configurar Body Parser
