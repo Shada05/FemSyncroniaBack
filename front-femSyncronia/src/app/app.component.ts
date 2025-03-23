e131087a8560f9659a61073e64ce2be2737e712c
Isaac Feliciano Hernández Rivera <Feliciano.Hernandez@alumno.udg.mx>
2025-03-23 12:10:48 -0600

    Ajustes en la redirección y en la pantalla validar codigo que no borraba el placeholder

--- front-femSyncronia/src/app/app.component.ts
+++ front-femSyncronia/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavController, Platform } from '@ionic/angular'; // Importa NavController y Platform
 import { AuthService } from './services/auth.service';
 import { PrimeraAperturaService } from './services/primera-apertura.service';
 
@@ -12,12 +12,18 @@ export class AppComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private primeraAperturaService: PrimeraAperturaService,
-    private router: Router
+    private navCtrl: NavController, // Inyecta NavController
+    private platform: Platform // Inyecta Platform
   ) { }
 
   async ngOnInit() {
     // Verifica si es la primera vez que se abre la aplicación
     //await this.verificarPrimeraApertura();
+
+    // Deshabilita el botón de retroceso físico en la pantalla de login
+    this.platform.backButton.subscribeWithPriority(10, () => {
+      // No hacer nada (deshabilitar el botón de retroceso)
+    });
   }
 
   async verificarPrimeraApertura() {
@@ -27,7 +33,7 @@ export class AppComponent implements OnInit {
 
     if (esPrimeraVez) {
       // Si es la primera vez, redirige a la pantalla de inicio
-      this.router.navigate(['/inicio']);
+      this.navCtrl.navigateRoot('/inicio'); // Usa navigateRoot para eliminar el historial
 
       // Marca que ya no es la primera vez
       await this.primeraAperturaService.asignar(false);
@@ -47,17 +53,17 @@ export class AppComponent implements OnInit {
       this.authService.verificarToken(token).subscribe(
         () => {
           // Si el token es válido, redirige a la pantalla principal
-          this.router.navigate(['/pantalla-principal']);
+          this.navCtrl.navigateRoot('/pantalla-principal'); // Usa navigateRoot para eliminar el historial
         },
         () => {
           // Si el token es inválido o expirado, redirige al login
           console.log('Token inválido o expirado, redirigiendo al login');
-          this.router.navigate(['/login']);
+          this.navCtrl.navigateRoot('/login'); // Usa navigateRoot para eliminar el historial
         }
       );
     } else {
       // Si no hay token almacenado, redirige al login
-      this.router.navigate(['/login']);
+      this.navCtrl.navigateRoot('/login'); // Usa navigateRoot para eliminar el historial
     }
   }
 }
