4073e7410368b099771f3ede313b18e269944c23
Isaac Feliciano Hernández Rivera <Feliciano.Hernandez@alumno.udg.mx>
2025-04-15 00:17:24 -0600

    URL ciclos cambiada
    se cambió la URL que se usaba para mostrar los ciclos

--- front-femSyncronia/src/app/services/api.service.ts
+++ front-femSyncronia/src/app/services/api.service.ts
@@ -34,7 +34,7 @@ export class ApiService {
   }
 
   mostrarCiclo(user_id: string): Observable<any>{
-    return this.http.get(`${this.apiUrl}/api/v1/cycles_tables_id/${user_id}`);
+    return this.http.get(`${this.apiUrl}/cycles_tables_id/${user_id}`);
   }
   
   crearCiclo(data:any): Observable<any>{
