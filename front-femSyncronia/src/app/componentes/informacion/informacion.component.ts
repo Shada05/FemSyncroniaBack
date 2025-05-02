4980d98721b01cf62aae65ede5bea58b776312c0
MallyMartinez <mallymartinez20@gmail.com>
2025-05-01 22:06:30 -0600

    Recomendación 1 e Imagenes
    información añadida de ¿Cuál debería elegir? e Imágenes correspondientes

--- front-femSyncronia/src/app/componentes/informacion/informacion.component.ts
+++ front-femSyncronia/src/app/componentes/informacion/informacion.component.ts
@@ -13,9 +13,9 @@ export class InformacionComponent  implements OnInit, OnDestroy {
     mostrarInfoOpciones = false;
   
     imagenesCarrusel: string[] = [
-      'assets/img/1ro-carrusel/flujo.svg',
-      'assets/img/1ro-carrusel/periodo.svg',
-      'assets/img/1ro-carrusel/versus.svg'
+      'assets/img/carrusel/flujo.svg',
+      'assets/img/carrusel/periodo.svg',
+      'assets/img/carrusel/versus.svg'
     ];
     
     slideVisual = 1;
@@ -97,9 +97,18 @@ cuidados = [
     { src: 'assets/img/recomendaciones/spotify.svg', alt: 'Spotify' },
     { src: 'assets/img/recomendaciones/calificanos.svg', alt: 'Califícanos' },
   ];
-  
-  
 
+  selectedRecomendacion: string | null = null;
+
+  verRecomendacion(titulo: string) {
+    this.selectedRecomendacion = titulo;
+  }
+
+  cerrarRecomendacion() {
+    this.selectedRecomendacion = null;
+  }
+
+  
   constructor() { }
 
 }
