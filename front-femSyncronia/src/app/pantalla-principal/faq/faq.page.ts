ef9b38e44423c507e64cc9b77385520c83a22a86
MallyMartinez <mallymartinez20@gmail.com>
2025-05-04 19:21:09 -0600

    Estructura de FAQ
    Solo falta información correspondiente en cada uno de los cuadros

--- front-femSyncronia/src/app/pantalla-principal/faq/faq.page.ts
+++ front-femSyncronia/src/app/pantalla-principal/faq/faq.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-faq',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  cerrarPantalla() {
+    this.navCtrl.back(); 
   }
+
+  ngOnInit() {}
 
 }
