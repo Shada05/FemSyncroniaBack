1b4d16b9ef46de630d9c35ec054c676f6195619a
MallyMartinez <mallymartinez20@gmail.com>
2025-05-08 00:57:45 -0600

    Pantalla Hidratate Listo

--- front-femSyncronia/src/app/pantalla-principal/hidratate/hidratate.page.ts
+++ front-femSyncronia/src/app/pantalla-principal/hidratate/hidratate.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-hidratate',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HidratatePage implements OnInit {
 
-  constructor() { }
+    constructor(private navCtrl: NavController) {}
+  
+    cerrarPantalla() {
+      this.navCtrl.back(); 
+    }
 
   ngOnInit() {
   }
