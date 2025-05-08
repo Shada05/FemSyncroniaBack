f9c7566e60279f3ad8ea6dec1afe98a5a990445c
MallyMartinez <mallymartinez20@gmail.com>
2025-05-08 00:34:25 -0600

    Rutas para cada imagen, falta para cada una

--- front-femSyncronia/src/app/pantalla-principal/inicio/inicio.page.ts
+++ front-femSyncronia/src/app/pantalla-principal/inicio/inicio.page.ts
@@ -8,6 +8,9 @@ import { ChangeDetectorRef } from '@angular/core';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { AlertController } from '@ionic/angular';
+import { IonTabs } from '@ionic/angular';
+import { ViewChild } from '@angular/core';
+
 
 interface Sintoma {
   id: number; // ID del síntoma
@@ -65,6 +68,11 @@ export class InicioPage implements OnInit {
   
   peso: number | null = null;
   temperatura: number | null = null;
+
+  irAPestanaInformacion() {
+    this.router.navigate(['/construccion']);
+  }
+  
 
   constructor(
     private menuCtrl: MenuController,
